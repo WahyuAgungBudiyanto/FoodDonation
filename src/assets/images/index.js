7cdd205000b6807020c234f0b803f1bcbd2878af
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2023-06-07 16:43:49 +0800

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import Background from './background.png'
+import Logo from './logo.png'
+import BackgroundSignIn from './backgroundSignIn.png'
+
+export {Background, Logo, BackgroundSignIn}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import SplashScreen from "./SplashScreen";
+import SignIn from "./SignIn";
+import HomeDonatur from "./HomeDonatur";
+import HomeKurir from "./HomeKurir";
+import HomeAdmin from "./HomeAdmin";
+
+export {SplashScreen, SignIn, HomeDonatur, HomeKurir, HomeAdmin}
